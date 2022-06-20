e8c070aa35c830b7dc627ab9a73134bd98483fcb
lorenzo.rizzolo@istitutoagnelli.it <lorenzo.rizzolo@istitutoagnelli.it>
2022-06-20 11:57:09 +0200

    jh

--- phpprog/index.php
+++ phpprog/index.php
@@ -2,7 +2,8 @@
     <div class="all">
     <h1>Puoi caricare uno o più file fino a 500Mb</h1>
     <p class="adv">Per selezionare più files tieni premuto <span style="text-decoration: underline;">Ctrl</span> durante la selezione. <br>
-    Se il file esiste già vi uscir&agrave; la scritta "il file esiste gi&agrave;!</p>
+    Se il file esiste già vi uscir&agrave; la scritta "il file esiste gi&agrave;! <br>
+Altri errori possono essere generati dal fatto che il file è troppo grande!</p>
     <link rel="stylesheet" href="css/style.css">
     <form method="post" enctype="multipart/form-data">
         <input type="file" name="file" multiple><br>
@@ -17,9 +18,11 @@
             $output = "";
 
             if(file_exists($target_file)){
-                $check = false;
-                $output = "Il file esiste già!";
+                $output = "Il file esiste già! <br> Ho provveduto a sostituirlo";
+                unlink($target_file);
             }
+            $ext = strtoupper(pathinfo($target_file,PATHINFO_EXTENSION));
+            
             if($_FILES['file']['name']==null){
                 $output = "<p class='adv'>Campo di caricamento vuoto</p>";
             }
@@ -28,7 +31,7 @@
                 if(!move_uploaded_file($_FILES['file']['tmp_name'],$target_file)){
                     echo "upload fallito";
                 }else{
-                    echo "file caricato!";
+                    echo $output."<br>file caricato!";
                 }
             }else{
                 echo $output;
@@ -42,7 +45,7 @@
         <p class="scan">La cartella contiene:</p>
         <?php
         foreach (glob("uploads/*") as $nomefile) {
-            echo  "<br>".$nomefile;
+            echo  "<br>".$nomefile."<a style='float:right; color:red; text-decoration:none; text-transform:uppercase;' href='".$nomefile."' download>Scarica</a>";
         }
         ?>
         </div>
