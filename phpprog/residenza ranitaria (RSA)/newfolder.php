9706c54550739359c8713fee7b00e97fc56a35b5
lorenzo-rizzolo <lorenzo.rizzolo@istitutoagnelli.it>
2022-06-22 08:21:44 +0200

    hv

--- phpprog/residenza ranitaria (RSA)/newfolder.php
+++ phpprog/residenza ranitaria (RSA)/newfolder.php
@@ -2,7 +2,9 @@
 echo "<a href='javascript:history.go(-1)' onMouseOver='self.status=document.referrer;return true'>&#8592;Back</a>";?>
 <form method="POST">
   <link rel="stylesheet" href="https://lorenzo-rizzolo.github.io/projects/phpprog/css/style.css">
-<input type="text" name="cartella" placeholder="Nome della nuova cartella">
+  <div class="increadir">
+<input class="cartella" type="text" name="cartella" placeholder="Nome della nuova cartella">
+</div>
 </form>
 <?php
     $cartella = $_POST['cartella'];
