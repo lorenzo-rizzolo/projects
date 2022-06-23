c932ab72bf54fd218742b291952044ec9ad01c1c
lorenzo-rizzolo <lorenzo.rizzolo@istitutoagnelli.it>
2022-06-23 11:24:26 +0200

    kghv

--- phpprog/index.php
+++ phpprog/index.php
@@ -57,7 +57,7 @@ Altri errori possono essere generati dal fatto che il file è troppo grande!</p>
                     echo  "<br><img src='https://lorenzo-rizzolo.github.io/projects/phpprog/css/file.png'><span name='".$n."'>".$nomefile."</span><a style='float:right; color:gold; text-decoration:none; text-transform:uppercase;' href='".$nomefile."' download>Scarica</a>";
                     $count += 1;
                 }else{
-                    echo "<br><img src='https://lorenzo-rizzolo.github.io/projects/phpprog/css/cartella.png'><a href='".$nomefile."/index.php'>Apri la cartella: '".$nomefile."'</a>";
+                    echo "<br><img src='https://lorenzo-rizzolo.github.io/projects/phpprog/css/cartella.png'><a href='".$nomefile."/index.php'>".$nomefile."</a>";
                 }
                
             }
