e69bb7c834c0feca1401ceca0d30eae3f7b1fee9
lorenzo.rizzolo@istitutoagnelli.it <lorenzo.rizzolo@istitutoagnelli.it>
2022-07-28 21:05:20 +0200

    jv

--- preventivi/preventivi/index.php
+++ preventivi/preventivi/index.php
@@ -4,14 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Preventivi</title>
 </head>
 <body>
+    <span id="line">Inserisci il prodotto, la quantit&agrave; e il prezzo unitario</span>
+    <hr>
     <div id="preve">
-
+        totale costo:
+        <hr>
+    <?php
+            for ($i=0; $i < 20; $i++) { 
+                $n=$i+50;
+                $t=$i+100;
+               echo "<input id='".$t."' type='text' placeholder='prodotto'></input><input id='".$i."' type='number' placeholder='quan.'></input><input id='".$n."' type='number' placeholder='p. unit.'></input><br>";
+            }
+    ?>
     </div>
-    <button class="add">
-        add line
-    </button>
+    
 </body>
 </html>
---
+++ preventivi/preventivi/script.js
@@ -0,0 +1,3 @@
+function add(){
+    document.getElementById('preve').innerHTML += "<input type='text'></input><input type='number'></input><br>"
+}
