4d11664d06cf2c6d74a7a01fb9b2339312e2f100
lorenzo.rizzolo@istitutoagnelli.it <lorenzo.rizzolo@istitutoagnelli.it>
2022-07-29 00:01:42 +0200

    jh

--- preventivi/preventivi/index.php
+++ preventivi/preventivi/index.php
@@ -5,21 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
     <title>Preventivi</title>
 </head>
 <body>
+    <script>totale()</script>
     <span id="line">Inserisci il prodotto, la quantit&agrave; e il prezzo unitario</span>
     <hr>
     <div id="preve">
-        totale costo:
+        totale costo: <span id="totale"></span><br>
+        pezzi totali: <span id="pezzi"></span><br>
+        <button onclick="two()">calcola</button>
+        
         <hr>
     <?php
-            for ($i=0; $i < 20; $i++) { 
-                $n=$i+50;
-                $t=$i+100;
-               echo "<input id='".$t."' type='text' placeholder='prodotto'></input><input id='".$i."' type='number' placeholder='quan.'></input><input id='".$n."' type='number' placeholder='p. unit.'></input><br>";
-            }
+        for ($i=0; $i < 20; $i++) { 
+            $n=$i+50;
+            $t=$i+100;
+            echo "<input id='".$t."' type='text' placeholder='prodotto'></input><input id='".$i."' type='number' placeholder='quan.'></input><input id='".$n."' type='number' placeholder='p. unit.'></input><br>";
+        }
     ?>
+    <script>totale()</script>
+    <script>pezzi()</script>
     </div>
     
 </body>
--- preventivi/preventivi/script.js
+++ preventivi/preventivi/script.js
@@ -1,3 +1,34 @@
-function add(){
-    document.getElementById('preve').innerHTML += "<input type='text'></input><input type='number'></input><br>"
+function two(){
+    totale();
+    pezzi();
+}
+
+function totale(){
+    var tot=0;
+    for (let i = 50; i < 70; i++) {
+        var num = document.getElementById(i).value;
+        var pez = document.getElementById(i-50).value;
+        if(pez=="" && num!=""){
+            pez=1;
+        }
+        num = num*pez;
+        if(num!=""){
+            tot = parseInt(parseInt(tot)+parseInt(num));
+        }
+    }
+    tot = parseInt(tot);
+    document.getElementById("totale").innerHTML = tot;
+}
+
+function pezzi(){
+    var pezzi=0;
+    for (let i = 0; i < 20; i++) {
+        var tot = document.getElementById(i+50).value;
+        var num = document.getElementById(i).value;
+        if(num!="" && tot!=""){
+            pezzi = parseInt(parseInt(pezzi)+parseInt(num));;
+        }
+    }
+    pezzi = parseInt(pezzi);
+    document.getElementById("pezzi").innerHTML = pezzi;
 }
